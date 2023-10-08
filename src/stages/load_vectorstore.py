929b8e8f236a8fcf0e6640df8d914a42be301b04
buesst1 <tobias.buess2001@gmail.com>
2023-10-08 16:41:33 +0200

    load vectorstore stage added

---
+++ src/stages/load_vectorstore.py
@@ -0,0 +1,35 @@
+"""
+Dieses Script is für das Laden einer Chroma Vektordatenbank verantwortlich.
+Auf dieser Datenbank kann eine Similarity-Query durchgeführt werden.
+
+Usage: script_name.py vector_database_filename(input) embedder_filename(input) query(input)
+"""
+
+from langchain.vectorstores import Chroma
+import argparse
+from dill import dump, load
+
+parser = argparse.ArgumentParser()
+
+#add positional arguments
+parser.add_argument('vector_database_filename')
+parser.add_argument('embedder_filename')
+parser.add_argument('query')
+
+args = parser.parse_args()
+
+# Access the arguments
+vector_database_filename = args.vector_database_filename
+embedder_filename = args.embedder_filename
+query = args.query
+
+#load embedder
+with open(embedder_filename, 'rb') as f:
+    embedder = load(f)
+
+# load chroma db
+db = Chroma(persist_directory=vector_database_filename, embedding_function=embedder)
+
+# query
+docs = db.similarity_search(query)
+print(docs[0].page_content)
