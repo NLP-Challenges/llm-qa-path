ff6550b38ee42f083dc0a12f81c380f51f2f7a9c
buesst1 <tobias.buess2001@gmail.com>
2023-10-08 16:41:19 +0200

    build vectorstore stage added

---
+++ src/stages/build_vectorstore.py
@@ -0,0 +1,36 @@
+"""
+Dieses Script is für das erstellen einer Chroma Vektordatenbank verantwortlich.
+Auf dieser Datenbank kann eine Similarity-Query durchgeführt werden.
+
+Usage: script_name.py corpus_filename(input) embedder_filename(input) vector_database_filename(output)
+"""
+
+from langchain.vectorstores import Chroma
+import argparse
+from dill import dump, load
+
+parser = argparse.ArgumentParser()
+
+#add positional arguments
+parser.add_argument('corpus_filename')
+parser.add_argument('embedder_filename')
+parser.add_argument('vector_database_filename')
+
+args = parser.parse_args()
+
+# Access the arguments
+corpus_filename = args.corpus_filename
+embedder_filename = args.embedder_filename
+vector_database_filename = args.vector_database_filename
+
+#load corpus
+with open(corpus_filename, 'rb') as f:
+    corpus = load(f)
+
+#load embedder
+with open(embedder_filename, 'rb') as f:
+    embedder = load(f)
+
+#create vectorstore and save it to vector_database_filename
+db = Chroma.from_texts(corpus, embedder, persist_directory=vector_database_filename)
+db.persist()
