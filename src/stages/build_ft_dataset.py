cd1641ea992d26eda8ffca006b1c00e2dc4de5ed
buesst1 <tobias.buess2001@gmail.com>
2023-10-15 17:14:24 +0200

    builder for llm fine tuning added

---
+++ src/stages/build_ft_dataset.py
@@ -0,0 +1,40 @@
+"""
+Dieses Script ist für das erstellen des llm fine-tuning datasets verantwortlich. Zum jetzigen Zeitpunkt wird das germanquad datenset (https://huggingface.co/datasets/deepset/germanquad) verwendet.
+Usage: script_name.py train_dataset_filename(output)
+"""
+
+import argparse
+from datasets import load_dataset
+import pandas as pd
+import datasets
+
+parser = argparse.ArgumentParser()
+
+#add positional arguments
+parser.add_argument('train_dataset_filename')
+
+args = parser.parse_args()
+
+# Access the arguments
+train_dataset_filename = args.train_dataset_filename
+
+
+#load germansquad dataset (train split) and convert to pandas dataframe
+df = pd.DataFrame(load_dataset("deepset/germanquad", split="train"))
+
+#remove additional context in context field like "Recht_der_Vereinigten_Staaten\n\n=== Amerikanisches Common Law ===\n'Actual context'"
+pattern = r'^.*?\n+\s*?={1,}.*?={1,}\s*?\n+' #pattern to search for
+filtered_df = df[df.context.str.contains(pattern)].copy() #only use observations containing this pattern
+filtered_df["context"] = filtered_df.context.str.replace(pattern, '', regex=True).str.replace("\n", " ") #remove those patterns
+
+#extract text from answer filed
+filtered_df["answers"] = filtered_df.answers.apply(lambda x: x["text"][0])
+
+#remove id filed
+filtered_df.drop(columns="id", inplace=True) 
+
+#reset index
+filtered_df.reset_index(drop=True, inplace=True)
+
+datasets.Dataset.from_pandas(filtered_df, split="train").save_to_disk(train_dataset_filename)
+
