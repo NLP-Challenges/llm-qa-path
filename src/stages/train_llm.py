fdc9c2d1d571b8287b21ed521c8eea7319cd59f5
Tobias Buess <tobias.buess2001@gmail.com>
2023-10-16 21:04:17 +0200

    working training pipeline

--- src/stages/train_llm.py
+++ src/stages/train_llm.py
@@ -1,7 +1,7 @@
 import argparse
 from datasets import load_from_disk
 import torch
-from transformers import AutoModelForCausalLM, BitsAndBytesConfig, AutoTokenizer, TrainingArguments
+from transformers import AutoModelForCausalLM, BitsAndBytesConfig, AutoTokenizer, TrainingArguments, PreTrainedModel
 from peft import LoraConfig
 from trl import SFTTrainer
 from dotenv import load_dotenv
@@ -49,25 +49,25 @@ train_dataset = load_from_disk(ft_dataset_filename)
 train_dataset = train_dataset.map(formatter)
 
 #according to https://medium.com/@ud.chandra/instruction-fine-tuning-llama-2-with-pefts-qlora-method-d6a801ebb19 & https://www.youtube.com/watch?v=eTieetk2dSw
+# Quantization Config
+quant_config = BitsAndBytesConfig(
+    load_in_8bit=True,
+    bnb_8bit_quant_type="nf4",
+    bnb_8bit_compute_dtype=torch.bfloat16,
+    bnb_8bit_use_double_quant=True
+)
 
 peft_config = LoraConfig(
     r=8,
-    lora_alpha=32,
+    lora_alpha=16,
     lora_dropout=0.1,
     bias="none",
     task_type="CAUSAL_LM",
 )
 
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.bfloat16,
-)
-
-base_model = AutoModelForCausalLM.from_pretrained(
+base_model:PreTrainedModel = AutoModelForCausalLM.from_pretrained(
     model_name,
-    quantization_config=bnb_config,
+    quantization_config=quant_config,
     device_map="cuda:0",
     token=hf_token
 )
@@ -76,17 +76,18 @@ tokenizer = AutoTokenizer.from_pretrained(
     model_name,
     token=hf_token
 )
-tokenizer.pad_token = tokenizer.eos_token
+tokenizer.pad_token = "[PAD]"
 tokenizer.padding_side = "right"
 
 training_args = TrainingArguments(
     output_dir="data/processed/training-output",
-    per_device_train_batch_size=2,
+    per_device_train_batch_size=1,
     gradient_accumulation_steps=4,
     learning_rate=2e-4,
     logging_steps=1,
-    max_steps=50,
+    max_steps=4,
     report_to="wandb",
+    optim="paged_adamw_32bit",
     fp16=True
 )
 
@@ -101,3 +102,7 @@ trainer = SFTTrainer(
 )
 
 trainer.train()
+
+#save model and tokenizer
+base_model.save_pretrained("data/models/llama2-test/model")
+tokenizer.save_pretrained("data/models/llama2-test/tokenizer")
