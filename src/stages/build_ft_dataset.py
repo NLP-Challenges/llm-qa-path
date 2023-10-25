8c3f753a7613a57be6747c58215a206e36f9db9f
Tobias Buess <tobias.buess2001@gmail.com>
2023-10-25 12:00:42 +0200

    training with unsuited questions to context
    - refuse to answer if question doesn't fit to context

--- src/stages/build_ft_dataset.py
+++ src/stages/build_ft_dataset.py
@@ -1,7 +1,8 @@
 """
 This script is responsible for creating the llm fine-tuning dataset. The germanquad dataset (https://huggingface.co/datasets/deepset/germanquad) is currently used.
+To allow the recognition of wrong context, the context was wapped for a couple of questions and the answer replaced.
 
-Usage: script_name.py train_dataset_filename(output)
+Usage: script_name.py seed(input) train_dataset_filename(output)
 """
 
 import argparse
@@ -9,17 +10,51 @@ from datasets import load_dataset
 import pandas as pd
 import datasets
 import time
+import numpy as np
+
+def swap_context(df:pd.DataFrame):
+    """ Copy df and swap the context randomly but also change answer
+    """
+    def shuffle(input):
+        copy = input.copy()
+
+        while True:
+            np.random.shuffle(copy)
+
+            if np.all(copy != input):
+                break
+
+        return copy
+        
+    grouped = df.groupby("context")
+
+    keys_from = list(grouped.groups.keys())
+    keys_to = shuffle(keys_from)
+
+    mapper = {key: value for key, value in zip(keys_from, keys_to)}
+
+    def swap(key, df):
+        df = df.copy()
+        df["context"] = [mapper[key]]*len(df)
+        df["answers"] = ["Leider liegen mir dazu keine Informationen vor"]*len(df)
+
+        return df
+
+    return pd.concat([swap(key, df) for (key, df) in grouped], ignore_index=True)
 
 parser = argparse.ArgumentParser()
 
 #add positional arguments
-parser.add_argument('dataset_filename')
+parser.add_argument('seed')
+parser.add_argument('train_dataset_filename')
 
 args = parser.parse_args()
 
 # Access the arguments
-dataset_filename = args.dataset_filename
+seed_ = int(args.seed)
+train_dataset_filename = args.train_dataset_filename
 
+np.random.seed(seed_) #set numpy seed
 
 #load germansquad dataset (train split) and convert to pandas dataframe
 df = pd.DataFrame(load_dataset("deepset/germanquad", split="train"))
@@ -38,7 +73,15 @@ filtered_df.drop(columns="id", inplace=True)
 #reset index
 filtered_df.reset_index(drop=True, inplace=True)
 
-datasets.Dataset.from_pandas(filtered_df, split="train").save_to_disk(dataset_filename)
+#swap context
+swapped = swap_context(filtered_df)
+
+#concat shortened swapped and stortened filteted df (can be deleted later) / shuffle
+final_df = pd.concat([filtered_df.sample(frac=0.7), swapped.sample(frac=0.3)], ignore_index=True).sample(frac = 1)
+
+print(final_df)
+
+datasets.Dataset.from_pandas(final_df, split="train").save_to_disk(train_dataset_filename)
 
 #wait a sec to avoid simulateous access to files
 time.sleep(1)
