40a821de16b3bb1c8dfd8b758efcd41187a07b74
Tobias Buess <tobias.buess2001@gmail.com>
2023-10-26 19:41:20 +0200

    training param changes / modifications
    - 13b model
    - removed unnecessary parameters at text generation
    - 'train on completion only' option added

--- src/stages/load_llm.py
+++ src/stages/load_llm.py
@@ -69,7 +69,12 @@ def predict(model:PreTrainedModel, tokenizer:LlamaTokenizer, question:str, conte
     )
 
     inputs = tokenizer(prompt, return_tensors="pt")
-    outputs = model.generate(input_ids=inputs["input_ids"].to(ft_model.device), attention_mask=inputs["attention_mask"], max_new_tokens=params["model_params"]["max_new_tokens"], pad_token_id=tokenizer.pad_token_id, do_sample=True, temperature=params["model_params"]["temperature"])
+    outputs = model.generate(
+        input_ids=inputs["input_ids"].to(ft_model.device),
+        max_new_tokens=params["model_params"]["max_new_tokens"], 
+        temperature=params["model_params"]["temperature"],
+        do_sample=True,
+    )
 
     return tokenizer.decode(outputs[:, inputs["input_ids"].shape[1]:][0], skip_special_tokens=True)
 
