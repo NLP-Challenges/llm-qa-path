ed62d98eaad1859a0ab0c49b90d04001fa6e4cfc
Yvo Keller <keller.yvo@gmail.com>
2023-11-18 00:07:12 +0100

    implement SelfQueryRetriever strategy

--- src/stages/load_vectorstore.py
+++ src/stages/load_vectorstore.py
@@ -6,6 +6,7 @@ Usage: script_name.py vector_database_filename(input) embedder_filename(input) q
 """
 
 from langchain.vectorstores import Chroma
+from dotenv import load_dotenv
 import argparse
 from dill import load
 import time
@@ -14,12 +15,18 @@ import tempfile
 import os
 import gc
 
+# env vars
+load_dotenv()
+
+# add positional arguments
 parser = argparse.ArgumentParser()
 
-#add positional arguments
 parser.add_argument('vector_database_filename')
 parser.add_argument('embedder_filename')
 parser.add_argument('query')
+
+# optional strategy argument
+parser.add_argument('--strategy', default='similarity', choices=['similarity', 'selfquery'])
 
 args = parser.parse_args()
 
@@ -27,6 +34,7 @@ args = parser.parse_args()
 vector_database_filename = args.vector_database_filename
 embedder_filename = args.embedder_filename
 query = args.query
+strategy = args.strategy
 
 def copy_folder(source_folder:str):
     # create a temporary folder
@@ -58,17 +66,56 @@ with open(embedder_filename, 'rb') as f:
 temp = copy_folder(vector_database_filename)
 
 # load chroma db
-db = Chroma(persist_directory=vector_database_filename, embedding_function=embedder)
+vectorstore = Chroma(persist_directory=vector_database_filename, embedding_function=embedder)
 
-# query
-docs = db.similarity_search(query)
+if strategy == 'similarity':
+    # query
+    docs = vectorstore.similarity_search(query)
 
-print(f"Found {len(docs)} documents")
-for doc in docs:
-    print(doc.metadata, doc.page_content)
+    print(f"Found {len(docs)} documents")
+    for doc in docs:
+        print(doc.metadata, doc.page_content)
+
+elif strategy == 'selfquery':
+    # advanced search
+    from langchain.chains.query_constructor.base import AttributeInfo
+    from langchain.llms.openai import OpenAI
+    from langchain.retrievers.self_query.base import SelfQueryRetriever
+
+    # Define our metadata
+    metadata_field_info = [
+        AttributeInfo(
+            name="module_name",
+            description="Name des Modul/Kurs/Space, auf den sich das Dokument bezieht",
+            type="string",
+        ),
+        AttributeInfo(
+            name="module_abbreviation",
+            description="Kurzbezeichnung des Modul/Kurs/Space, um das es in dem Dokument geht",
+            type="string",
+        ),
+        AttributeInfo(
+            name="source",
+            description="Tab innerhalb des Moduls/Kurses/Space, aus dem das Dokument stammt",
+            type="string",
+        ),
+    ]
+    document_content_description = "Informationen aus Spaces, der Lernplattform des Studiengang Bachelor of Data Science an der FHNW"
+
+    # Define self query retriever
+    llm = OpenAI(temperature=0)
+    retriever = SelfQueryRetriever.from_llm(
+        llm, vectorstore, document_content_description, metadata_field_info, verbose=True
+    )
+
+    docs = retriever.get_relevant_documents(query)
+
+    print(f"Found {len(docs)} documents")
+    for doc in docs:
+        print(doc.metadata, doc.page_content)
 
 # disconnect from chroma
-del db
+del vectorstore
 gc.collect()
 
 # wait a sec to avoid simulateous access to files
