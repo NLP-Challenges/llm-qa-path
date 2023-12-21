98d233b623cd26ab12932a9df1ca579fc8c18a0d
Tobias Buess <tobias.buess2001@gmail.com>
2023-12-21 10:01:49 +0100

    modified build_ft_dataset stage
    - stratified random sampling
    - leakage aware sampling

--- src/stages/build_ft_dataset.py
+++ src/stages/build_ft_dataset.py
@@ -2,7 +2,7 @@
 This script is responsible for creating the llm fine-tuning dataset. The germanquad dataset (https://huggingface.co/datasets/deepset/germanquad) is currently used.
 To allow the recognition of wrong context, the context was wapped for a couple of questions and the answer replaced.
 
-Usage: script_name.py params_parent_field(input) train_dataset_filename(output)
+Usage: script_name.py params_parent_field(input) dataset_filename(output)
 """
 
 import argparse
@@ -52,17 +52,37 @@ def swap_context(df:pd.DataFrame):
 
     return pd.concat([swap(key, df) for (key, df) in grouped], ignore_index=True)
 
+def sample(df_original:pd.DataFrame, df_swapped:pd.DataFrame, size:int, fraw_swapped:float):
+    n_original = int(size * (1-fraw_swapped)) #calculate number of samples from original dataframe
+    n_swapped = size - n_original #calculate number of samples from swapped dataframe
+
+    #sample from dataframes
+    split_original = df_original.sample(n=n_original, random_state=params["seed"])
+    split_swapped = df_swapped.sample(n=n_swapped, random_state=params["seed"])
+
+    #combine
+    split = pd.concat([split_original, split_swapped], ignore_index=True)
+
+    #get unique contexts
+    used_context = split.context.unique()
+
+    #drop all observations with this used context from original and swapped dataframe
+    df_original.drop(df_original[df_original.context.isin(used_context)].index, inplace=True)
+    df_swapped.drop(df_swapped[df_swapped.context.isin(used_context)].index, inplace=True)
+
+    return split #return split
+
 parser = argparse.ArgumentParser()
 
 #add positional arguments
 parser.add_argument('params_parent_field')
-parser.add_argument('train_dataset_filename')
+parser.add_argument('dataset_filename')
 
 args = parser.parse_args()
 
 # Access the arguments
 params_parent_field = args.params_parent_field
-train_dataset_filename = args.train_dataset_filename
+dataset_filename = args.dataset_filename
 
 # parse params
 with open("params.yaml", 'r') as file:
@@ -71,43 +91,58 @@ with open("params.yaml", 'r') as file:
 #set numpy seed (for swap_context)
 np.random.seed(params["seed"]) 
 
-#load germansquad dataset (train split) and convert to pandas dataframe
-df = pd.DataFrame(load_dataset("deepset/germanquad", split="train"))
+# concatenate training and test split
+df_original:pd.DataFrame = pd.concat(
+    [pd.DataFrame(load_dataset("deepset/germanquad")["train"]), pd.DataFrame(load_dataset("deepset/germanquad")["test"])],
+    ignore_index=True,
+)
 
 #remove additional context in context field like "Recht_der_Vereinigten_Staaten\n\n=== Amerikanisches Common Law ===\n'Actual context'"
 pattern = r'^.*?\n+\s*?={1,}.*?={1,}\s*?\n+' #pattern to search for
-filtered_df = df[df.context.str.contains(pattern)].copy() #only use observations containing this pattern
-filtered_df["context"] = filtered_df.context.str.replace(pattern, '', regex=True).str.replace("\n", " ") #remove those patterns
+df_original = df_original[df_original.context.str.contains(pattern)] #only use observations containing this pattern
+df_original["context"] = df_original.context.str.replace(pattern, '', regex=True).str.replace("\n", " ") #remove those patterns
 
 #extract text from answer filed
-filtered_df["answers"] = filtered_df.answers.apply(lambda x: x["text"][0])
+df_original["answers"] = df_original.answers.apply(lambda x: x["text"][0])
 
 #remove id filed
-filtered_df.drop(columns="id", inplace=True) 
+df_original.drop(columns="id", inplace=True) 
 
 #reset index
-filtered_df.reset_index(drop=True, inplace=True)
+df_original.reset_index(drop=True, inplace=True)
 
 #add new column for "can be answered"
-filtered_df["can_be_answered"] = [True]*len(filtered_df)
+df_original["can_be_answered"] = [True]*len(df_original)
 
-print("total number of question, context, answer pairs available: ", len(filtered_df))
+print("total number of question, context, answer pairs available: ", len(df_original))
 
 #swap context
-swapped = swap_context(filtered_df)
+df_swapped = swap_context(df_original)
 
-df_frac_original = filtered_df.sample(n=params["n_original"], frac=params["frac_original"], random_state=params["seed"])
-df_frac_swapped = swapped.sample(n=params["n_swapped"], frac=params["frac_swapped"], random_state=params["seed"])
+## sampling
+train_split = sample(df_original, df_swapped, params["n_train"], params["frac_swapped"])
+val_split = sample(df_original, df_swapped, params["n_val"], params["frac_swapped"])
+test_split = sample(df_original, df_swapped, params["n_test"], params["frac_swapped"])
 
-print("number of question, context, answer pairs: ", len(df_frac_original))
-print("number of non fitting question, context, answer pairs: ", len(df_frac_swapped))
+#add new column which identifies the split
+train_split["split"] = ["train"]*len(train_split)
+val_split["split"] = ["val"]*len(val_split)
+test_split["split"] = ["test"]*len(test_split)
 
-#concat shortened original and shortened swapped.
-final_df = pd.concat([df_frac_original, df_frac_swapped], ignore_index=True).sample(frac = 1, random_state=params["seed"]).reset_index(drop=True, inplace=False)
+if len(np.intersect1d(train_split.context.unique(), val_split.context.unique())) > 0:
+    raise Exception("Leakage between train_split and val_split")
+
+if len(np.intersect1d(train_split.context.unique(), test_split.context.unique())) > 0:
+    raise Exception("Leakage between train_split and test_split")
+
+if len(np.intersect1d(val_split.context.unique(), test_split.context.unique())) > 0:
+    raise Exception("Leakage between val_split and test_split")
+
+final_df = pd.concat([train_split, val_split, test_split]).sample(frac = 1, random_state=params["seed"]).reset_index(drop=True, inplace=False)
 
 print("final dataset length: ", len(final_df))
 
-datasets.Dataset.from_pandas(final_df, split="train").save_to_disk(train_dataset_filename)
+datasets.Dataset.from_pandas(final_df, split="train").save_to_disk(dataset_filename)
 
 #wait a sec to avoid simulateous access to files
 time.sleep(1)
